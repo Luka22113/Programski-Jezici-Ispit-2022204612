ca1676f63016af76865ca2d4061c86db1c0878e2
Luka22113 <luka.knezevic1890@gmail.com>
2024-07-04 11:14:23 +0200

    da

--- README.md
+++
@@ -1,2 +0,0 @@
-# Programski-Jezici-Ispit-2022204612
-Program za ispit
--- src/main/java/luka/knezevic/LukaKnezevic2022204612/DTO/KupovinaDTO.java
+++
@@ -1,8 +0,0 @@
-package luka.knezevic.LukaKnezevic2022204612.DTO;
-
-import java.time.LocalDateTime;
-
-public class KupovinaDTO {
-    public int brojPeciva;
-    public Long pecivoId;
-}
