082cc26fd240dfe702be8cef765364beff8bee50
Luka22113 <luka.knezevic1890@gmail.com>
2024-07-04 11:06:22 +0200

    Initial commit

---
+++ src/test/java/luka/knezevic/LukaKnezevic2022204612/LukaKnezevic2022204612ApplicationTests.java
@@ -0,0 +1,13 @@
+package luka.knezevic.LukaKnezevic2022204612;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukaKnezevic2022204612ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
